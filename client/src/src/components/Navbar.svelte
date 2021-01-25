5d16b6f99dc02b25c611c6b4f1ac926986997de9
Phaqui <Phaqui@gmail.com>
2021-01-25 03:40:24 +0100

    Add GitHub logo with link to repo

--- client/src/src/components/Navbar.svelte
+++ client/src/src/components/Navbar.svelte
@@ -3,16 +3,22 @@
 </script>
 
 <nav>
-  <a use:link href="/">Landing</a>
-  <a use:link href="/explanation">What</a>
-  <a use:link href="/submit">Submit</a>
-  <a use:link href="/stats">Stats</a>
+  <a class="menuitem" use:link href="/">Llamalucky</a>
+  <a class="menuitem" use:link href="/explanation">What</a>
+  <a class="menuitem" use:link href="/submit">Submit</a>
+  <a class="menuitem" use:link href="/stats">Stats</a>
+  <a class="push" href="https://github.com/phaqui">
+    <img src="/GitHub-Mark-64px.png" alt="GitHub Mark" />
+  </a>
 </nav>
 
 <style>
   nav {
     background-color: rgb(166, 49, 49);
-    padding: 2vh 0;
+    padding: 1.5vh 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   nav a {
@@ -23,10 +29,27 @@
     font-weight: bold;
   }
 
-  nav a::before {
+  nav a:last-child {
+    margin-right: 0.6em;
+  }
+
+  nav a.menuitem::before {
     content: '•';
     text-decoration: none;
-    padding-left: 0.9em;
+    padding-left: 0.6em;
     color: rgb(50, 50, 50);
+  }
+
+  nav a.push {
+    margin-left: auto;
+  }
+
+  nav a img {
+    height: 3.5vh;
+    filter: drop-shadow(0px 0px 20px #c2c0d0);
+  }
+
+  nav a img:hover {
+    filter: drop-shadow(0 0 0 white);
   }
 </style>
